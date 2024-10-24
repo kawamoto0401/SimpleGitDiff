733d2a1d681ccf51fd346dee646f8c2968e6b298
Kawamoto <kawamoto0401@outlook.jp>
2024-10-25 00:03:03 +0900

    0.0.2 試作

--- README.md
+++ README.md
@@ -1,71 +1,20 @@
 # simplegitdiff README
 
-This is the README for your extension "simplegitdiff". After writing up a brief description, we recommend including the following sections.
+簡単に現在の状態と指定のコミットIDを比較して、表示するプログラム
 
 ## Features
 
-Describe specific features of your extension including screenshots of your extension in action. Image paths are relative to this README file.
+VSCodeで開いている状態のパスを取得して
 
-For example if there is an image subfolder under your extension project workspace:
+  git diff [コミットID] ファイルパス
 
-\!\[feature X\]\(images/feature-x.png\)
+を行い編集履歴を取得
 
-> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
+それを画面に反映させます
 
-## Requirements
+単体テストをするときに、変更箇所を探すのが面倒(コミット結合。。。比較画面があってもブレークを置けなかったので)だったので作りました
 
-If you have any requirements or dependencies, add a section describing those and how to install and configure them.
+コマンド
 
-## Extension Settings
-
-Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
-
-For example:
-
-This extension contributes the following settings:
-
-* `myExtension.enable`: Enable/disable this extension.
-* `myExtension.thing`: Set to `blah` to do something.
-
-## Known Issues
-
-Calling out known issues can help limit users opening duplicate issues against your extension.
-
-## Release Notes
-
-Users appreciate release notes as you update your extension.
-
-### 1.0.0
-
-Initial release of ...
-
-### 1.0.1
-
-Fixed issue #.
-
-### 1.1.0
-
-Added features X, Y, and Z.
-
----
-
-## Following extension guidelines
-
-Ensure that you've read through the extensions guidelines and follow the best practices for creating your extension.
-
-* [Extension Guidelines](https://code.visualstudio.com/api/references/extension-guidelines)
-
-## Working with Markdown
-
-You can author your README using Visual Studio Code. Here are some useful editor keyboard shortcuts:
-
-* Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux).
-* Toggle preview (`Shift+Cmd+V` on macOS or `Shift+Ctrl+V` on Windows and Linux).
-* Press `Ctrl+Space` (Windows, Linux, macOS) to see a list of Markdown snippets.
-
-## For more information
-
-* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
-* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
-
-**Enjoy!**
+  SimpleGitDiff                 表示
+  SimpleGitDiffClear            非表示
--- src/extension.ts
+++ src/extension.ts
@@ -4,6 +4,7 @@ import { promises } from 'dns';
 import path from 'path';
 import SimpleGit  from 'simple-git';
 import * as vscode from 'vscode';
+import { TextEditorDecorationType } from "vscode";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -16,13 +17,11 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('simplegitdiff.helloWorld', async() => {
+	let disposable = vscode.commands.registerCommand('simplegitdiff.makeSimpleGitDiff', async() => {
 
 		const editor = vscode.window.activeTextEditor;
 
 		if( editor ) {
-
-			
 			function splitFilePath(filePath: string): { dir: string, base: string } {
 				const dir = path.dirname(filePath);
 				const base = path.basename(filePath);
@@ -34,7 +33,7 @@ export function activate(context: vscode.ExtensionContext) {
 			const result = splitFilePath(filePath);
 
 			// コミット番号を取得する
-			const commitId = await vscode.window.showInputBox({title: 'Commit No?'});
+			const commitId = await vscode.window.showInputBox({title: 'Please specify the commitID'});
 			if( commitId !== undefined ) {
 				getSimpleGitDiff(result.dir, result.base, commitId);
 			}else {
@@ -47,6 +46,55 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 
 	context.subscriptions.push(disposable);
+
+	disposable = vscode.commands.registerCommand('simplegitdiff.makeSimpleGitDiffClear', () => {
+
+
+	});
+
+	context.subscriptions.push(disposable);
+
+	// アクティブなエディターが変更されたときに発生するイベント
+	vscode.window.onDidChangeActiveTextEditor(textEditor => {
+		console.log("### updateEditorDecorations ");
+
+		if (typeof textEditor === "undefined") {
+            return;
+        }
+
+        let fsPath = textEditor.document.uri.fsPath;
+
+		// 削除するためのTextEditorDecorationTypeをファイルパス：行番号で管理
+		let gutterIconMap: Map<string, vscode.TextEditorDecorationType>;
+
+    	gutterIconMap = new Map();
+
+        let editorDecorations = new Map<TextEditorDecorationType, vscode.Range[]>();
+        for (let [filePathLineNumber, decoration] of gutterIconMap) {
+
+            let filePath = "";
+            let lineNumber = Number('0');
+
+            let ranges = editorDecorations.get(decoration);
+            if (typeof ranges === "undefined") {
+                ranges = new Array<vscode.Range>();
+                editorDecorations.set(decoration, ranges);
+            }
+
+            ranges.push(new vscode.Range(lineNumber, 0, lineNumber, 0));
+        }
+
+        for (let [decoration, ranges] of editorDecorations) {
+            textEditor.setDecorations(decoration, ranges);
+        }
+	});
+
+	// テキストドキュメントが変更されたときに発行されるイベント
+	vscode.workspace.onDidChangeTextDocument(textDocumentChangeEvent => {
+        let fsPath = textDocumentChangeEvent.document.uri.fsPath;
+
+		// 不要
+	});
 }
 
 
@@ -58,7 +106,7 @@ async function getSimpleGitDiff(dir: string, fileName: string, commandId: string
 
 		let logVersion = await git.version();
 		if(!logVersion.installed) {
-			vscode.window.showInformationMessage('git no install');
+			vscode.window.showInformationMessage('git is not install');
 			return;
 		}
 
@@ -66,14 +114,14 @@ async function getSimpleGitDiff(dir: string, fileName: string, commandId: string
 		if( commandId.length === 0) {
 			logLog = await git.log([fileName]);
 			if( logLog.all.length === 0 ) {
-				vscode.window.showInformationMessage('non log');
+				vscode.window.showInformationMessage('git log failed');
 				return;
 			}
 		}
 		else {
 			logLog = await git.log([fileName, commandId]);
 			if( logLog.all.length === 0 ) {
-				vscode.window.showInformationMessage('non find');
+				vscode.window.showInformationMessage('I cannot find the commitID');
 				return;
 			}
 		}
@@ -87,7 +135,7 @@ async function getSimpleGitDiff(dir: string, fileName: string, commandId: string
 		}
 
 		if( logDiff.length === 0 ) {
-			vscode.window.showInformationMessage('non diff');
+			vscode.window.showInformationMessage('git diff failed');
 			return;
 		}
 
@@ -98,7 +146,7 @@ async function getSimpleGitDiff(dir: string, fileName: string, commandId: string
 	}
 	catch(error) {
 		console.log(error);
-	}	
+	}
 }
 
 // This method is called when your extension is deactivated
